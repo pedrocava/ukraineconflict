08ac873d432d4de5b0cbec731178345d6ef2a608
Pedro Cavalcante Oliveira <pedrocolrj@gmail.com>
2025-03-08 21:23:16 -0300

    Update utils-pipe.R

--- R/utils-pipe.R
+++ R/utils-pipe.R
@@ -18,11 +18,26 @@ NULL
 #' See \code{magrittr::\link[magrittr:pipe]{\%$\%}} for details.
 #'
 #' @name %$%
-#' @rdname pipe
+#' @rdname explosion_pipe
 #' @keywords internal
 #' @export
 #' @importFrom magrittr %$%
 #' @usage lhs \%$\% rhs
+#' @param lhs A value or the magrittr placeholder.
+#' @param rhs A function call using the magrittr semantics.
+#' @return The result of calling `rhs(lhs)`.
+NULL
+
+#' Side-effect operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%T>\%}} for details.
+#'
+#' @name %T>%
+#' @rdname side_effect_pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %T>%
+#' @usage lhs \%T>\% rhs
 #' @param lhs A value or the magrittr placeholder.
 #' @param rhs A function call using the magrittr semantics.
 #' @return The result of calling `rhs(lhs)`.
