271c3ec2aba406ae7f468c2cc4ad0972e373907b
Pedro Cavalcante Oliveira <pedrocolrj@gmail.com>
2025-03-09 21:59:54 -0300

    Create timeline.R

---
+++ R/timeline.R
@@ -0,0 +1,20 @@
+#' @export
+#' @name war_phase
+
+war_phase <- function(timestamp) {
+
+  dplyr::case_when(
+    lubridate::date(timestamp) <= lubridate::dmy("10-04-2022") ~
+      "Early Days",
+    lubridate::date(timestamp) <= lubridate::dmy("30-08-2022") ~
+      "First Donbass Campaign",
+    lubridate::date(timestamp) <= lubridate::dmy("11-11-2022") ~
+      "2022 Ukranian Counteroffensives",
+    lubridate::date(timestamp) <= lubridate::dmy("07-06-2023") ~
+      "First Stalemate",
+    lubridate::date(timestamp) <= lubridate::dmy("30-11-2023") ~
+      "2023 Ukranian Counteroffensives",
+    lubridate::date(timestamp) <= lubridate::today() ~
+      "Second Stalemate")
+
+}
